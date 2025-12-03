6a1c4776092932a9da5ed74bc58ecab013ee1c2a
carmenfdez10 <carmenzrgn13@gmail.com>
2025-12-03 10:11:30 +0100

    ctnginxbase

--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -19,8 +19,9 @@ main() {
         newSSH
     fi
 
-    # encargada de dejar este contendor vivo en BGround
-    ##tail -f /dev/null
+    # encargada de dejar este contendor vivo en BGround. 
+    # Eliminamos el tail 
+    ## tail -f /dev/null
     ## script's que se encargar de configurar el imagen/contenedor
 }
 
